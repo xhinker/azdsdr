9d69c27320c9226673937ac795786bbf0961bfd7
Andrew Zhu <xhinker@hotmail.com>
2022-09-29 10:23:14 -0700

    path tune

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup,find_packages
 
 setup(
     name='azdsdr',
-    version='1.220929',
+    version='1.220929.2',
     license='Apache License',
     author="Andrew Zhu",
     author_email='xhinker@hotmail.com',
     packages=find_packages('src'),
-    package_dir={'': 'src'},
+    package_dir={'': 'src/azdsdr'},
     url='https://github.com/xhinker/azdsdr',
-    keywords='example project',
+    keywords='DS Data Reader',
     install_requires=[
           'pandas'
           ,'pyodbc'
