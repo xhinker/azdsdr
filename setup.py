3c3367061f9a3d98e03ff6261cd5a82eaaa9f51c
Andrew Zhu <xhinker@hotmail.com>
2022-09-29 10:40:06 -0700

    update config

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup,find_packages
 
 setup(
     name='azdsdr',
-    version='1.220929.2',
+    version='1.220929.5',
     license='Apache License',
     author="Andrew Zhu",
     author_email='xhinker@hotmail.com',
     packages=find_packages('src'),
-    package_dir={'': 'src/azdsdr'},
+    package_dir={'': 'src'},
     url='https://github.com/xhinker/azdsdr',
     keywords='DS Data Reader',
     install_requires=[
