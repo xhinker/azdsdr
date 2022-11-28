966e2ead4e5de5b47156dc64c58e88d8919cab49
Andrew Zhu <anzhu@microsoft.com>
2022-11-28 13:57:26 -0800

    auto commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='azdsdr',
-    version='1.221118.1',
+    version='1.221128.1',
     license='Apache License',
     author="Andrew Zhu",
     author_email='xhinker@hotmail.com',
